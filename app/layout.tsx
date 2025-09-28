c462a99ec9176ba3ba6fae13947047ccd62938e0
ZainabShujat <zainabshujatali@gmail.com>
2025-09-28 12:49:37 +0530

    corrected layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,11 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      
+       <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+  {children}
+</body>
     </html>
   );
 }
