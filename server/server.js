6c556c8aa8e52a0c5f1e6b8bfd38c6c18375e435
ZainabShujat <zainabshujatali@gmail.com>
2025-10-27 19:14:56 +0530

    added server.js code to connect to cluster0

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
+// --- connect to MongoDB Atlas ---
+const MONGO_URI = "mongodb+srv://zainabshujatali_db_user:Vd7dNDwkaKKPT58q@cluster0.ny94mj4.mongodb.net/?appName=Cluster0";
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
+
+// --- basic route ---
 app.get("/", (req, res) => {
-  res.send("Hello Zee, your server works 🚀");
+  res.send("Hello Zee, your server & database are alive 🚀");
 });
 
 app.listen(4000, () => {
