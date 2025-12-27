c15b07b166f13b1c6ab84bcd6607103d95f1aedc
ZainabShujat <zainabshujatali@gmail.com>
2025-12-27 23:36:57 +0530

    add again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** next.config.js */
 module.exports = {
-  // No experimental turbopack key!
-    turbopack: {}, // Silences Turbopack warning per Next.js docs
     webpack: (config) => {
       return config;
     },
